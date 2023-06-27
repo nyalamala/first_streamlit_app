d8880c377e3a765050dfe0c003132ea638897380
nyalamala <136059289+nyalamala@users.noreply.github.com>
2023-06-27 15:37:29 +0530

    Update streamlit_app.py
    changed title to text for menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title("My Parents new healthy diner ")
 
 streamlit.header("Breakfast Menu ")
-streamlit.title("omenga 3 and Blueberry oatmeal")
-streamlit.title("Kale , Spinch and Rocket Smoothie ")
-streamlit.title("Hard Boiled Free Range Egg")
+streamlit.text("omenga 3 and Blueberry oatmeal")
+streamlit.text("Kale , Spinch and Rocket Smoothie ")
+streamlit.text("Hard Boiled Free Range Egg")
