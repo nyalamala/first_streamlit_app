6ff9891374601fab6a81a6a62adc49cc279f1295
nyalamala <136059289+nyalamala@users.noreply.github.com>
2023-06-28 16:53:05 +0530

    Update streamlit_app.py
    removed the plain json data and separated the fruity api

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-#new section to display fruits
+#new section to display fruits ================================================
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"Kiwi")
+
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
