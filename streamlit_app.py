dd2bd6e3f208b6e49cf485260b36ac1f3d8da60a
nyalamala <136059289+nyalamala@users.noreply.github.com>
2023-06-29 14:57:23 +0530

    Update streamlit_app.py
    enhancing fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,8 @@ my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT * from FRUIT_LOAD_LIST")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains ")
-streamlit.text(my_data_row)
+streamlit.header("The fruit load list contains :")
+streamlit.dataframe(my_data_row)
 
 
 
