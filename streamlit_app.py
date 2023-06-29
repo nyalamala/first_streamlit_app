91f363c891ac5836dd91f72ba9038ed3c7e26f6a
nyalamala <136059289+nyalamala@users.noreply.github.com>
2023-06-29 15:19:00 +0530

    Update streamlit_app.py
    added insert statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,27 +15,21 @@ streamlit.text("🐔 Hard Boiled Free Range Egg")
 streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
 #new section to display fruits ================================================
 streamlit.header("Fruityvice Fruit Advice!")
-
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
@@ -51,6 +45,28 @@ streamlit.dataframe(my_data_row)
 
 
 #allow end user to add fruit in the list
-
 add_my_fruit=streamlit.text_input('What fruit would you like to add : ','jackfruit')  
 streamlit.write('Thanks for adding  ', add_my_fruit)
+
+
+#this will not work properly just go with the flow
+my_cur.execute("insert into FRUIT_LOAD_LIST values('from streamlit')")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
